30be7e235666a6bc40081d52b6b03f300b079ffe
pajic2017 <pajic.it.2017@gmail.com>
2021-11-27 20:48:27 +0100

    Second message

--- src/main/java/com/paki/testfour/WelcomeTest.java
+++ src/main/java/com/paki/testfour/WelcomeTest.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeTest {
 
+  //show us how to return String class
    @GetMapping("/showWorld")
     public String showData(){
        return "Hello World !!!";
