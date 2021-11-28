2842979251eb7de883bd91d17918c0ed87384df8
pajic2017 <pajic.it.2017@gmail.com>
2021-11-28 20:22:37 +0100

    Fifth message in App Arhitecture

--- src/main/java/com/paki/testfour/TestFourApplication.java
+++ src/main/java/com/paki/testfour/TestFourApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestFourApplication {
 
-   //most important class, main is always key
+   //most important class, main is always key test
     public static void main(String[] args) {
 
         SpringApplication.run(TestFourApplication.class, args);
