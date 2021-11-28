2194e223ce0982b22a44ac5af59b3f3e05608b38
pajic2017 <pajic.it.2017@gmail.com>
2021-11-28 19:55:43 +0100

    Fourth message in App Arhitecture

--- src/main/java/com/paki/testfour/WelcomeTest.java
+++ src/main/java/com/paki/testfour/WelcomeTest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeTest {
 
-  //show us how to return String class
+  //show us how to return String class, and bind to Git and after use Jenkins
    @GetMapping("/showWorld")
     public String showData(){
        return "Hello World !!!";
